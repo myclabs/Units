daffc76bd8a2ced0406c0ebf5fc6d680a52795ed
Matthieu Napoli <matthieu@mnapoli.fr>
2013-12-23 11:19:44 +0100

    Multilingue pour le site web

--- src/MyCLabs/UnitBundle/Controller/DefaultController.php
+++ src/MyCLabs/UnitBundle/Controller/DefaultController.php
@@ -3,9 +3,21 @@
 namespace MyCLabs\UnitBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    public function localeRedirectionAction(Request $request)
+    {
+        $lang = locale_get_primary_language($request->getPreferredLanguage());
+
+        if (! in_array($lang, $this->container->getParameter('available_locales'))) {
+            $lang = $this->container->getParameter('default_locale');
+        }
+
+        return $this->redirect($this->generateUrl('homepage', ['_locale' => $lang]));
+    }
+
     public function homeAction()
     {
         return $this->render('UnitBundle:Default:home.html.twig');
