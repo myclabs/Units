7d0e828f4e8765d0eaa4b1bb8577aa51433283e8
Matthieu Napoli <matthieu@mnapoli.fr>
2014-05-26 15:51:04 +0200

    Utilisation de TranslatedString

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,6 @@
 <?php
 
 use Doctrine\ORM\EntityManager;
-use Mnapoli\Translated\Translator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -10,7 +9,4 @@ return [
     LoggerInterface::class => DI\link('logger'),
     EntityManager::class => DI\link('doctrine.orm.entity_manager'),
     TranslatorInterface::class => DI\link('translator'),
-
-    Translator::class => DI\object()
-        ->constructor(DI\link('locale')),
 ];
